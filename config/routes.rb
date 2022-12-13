fe3ebf1be9bf25f0f29d50c2372867aa37fff118
Abbas Nazarov <abbas.nazarov@gmail.com>
2022-12-13 12:57:26 +0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
   resources :contacts, only: [:index, :show]
   get 'walnuts/index'
   get 'walnuts/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root to: "walnuts#index"
 end
